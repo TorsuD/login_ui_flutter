4a90e25bed7ca1fef38392345b74854b800c81d3
Derrick Torsu <torsuderrick2004@gmail.com>
2022-10-18 20:22:02 +0000

    first flutter project

---
+++ lib/continue.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:login_flutter/login.dart';
+import 'package:login_flutter/signup.dart';
+
+class ContinuePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // The SKIP button
+          GestureDetector(
+            onTap: (() {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return Login();
+                },
+              ));
+            }),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 35.0,
+                    width: 60.0,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 243, 235, 213),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                      child: Text(
+                        "Skip",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // The Image
+          Container(
+            margin: const EdgeInsets.all(50.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100.0),
+              color: const Color.fromARGB(255, 238, 238, 238),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Image.asset(
+                "../assets/login.png",
+                height: 180.0,
+              ),
+            ),
+          ),
+
+          // The Button Section
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  height: 40.0,
+                  width: 450.0,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: const Text("Continue with Email")),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  height: 40.0,
+                  width: 450.0,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: () {},
+                      child: const Text("Continue with Apple")),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  height: 40.0,
+                  width: 450.0,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                      onPressed: () {},
+                      child: const Text("Continue with Google")),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  height: 40.0,
+                  width: 450.0,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 0, 82, 148),
+                      ),
+                      onPressed: () {},
+                      child: const Text("Continue with Facebook")),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account? "),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return SignUp();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                const Text(
+                    "By clicking 'Continue with Email/Apple/Google/Facebook'",
+                    style: TextStyle(fontSize: 10.0)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("above, you agree to",
+                        style: TextStyle(fontSize: 10.0)),
+                    Text("Terms and Conditions ",
+                        style: TextStyle(color: Colors.amber, fontSize: 10.0)),
+                    Text("and", style: TextStyle(fontSize: 10.0)),
+                    Text(
+                      " Privacy Policy",
+                      style: TextStyle(color: Colors.amber, fontSize: 10.0),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // The AppBar Section with arrow and 'Sign in' text
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100.0),
+                    color: Colors.blue.shade100),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                ),
+              ),
+              const Text(
+                "Sign in",
+                style: TextStyle(fontWeight: FontWeight.w700),
+              )
+            ],
+          ),
+
+          // The Login Image
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+            height: 100.0,
+            child: Image.asset("../assets/women.png"),
+          ),
+
+          // The Input Fields
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color.fromARGB(255, 229, 243, 255),
+            ),
+            margin: const EdgeInsets.all(10.0),
+            child: const TextField(
+              style: TextStyle(
+                fontSize: 12,
+              ),
+              decoration: InputDecoration(
+                fillColor: Colors.red,
+                border: InputBorder.none,
+                labelText: 'Enter your email',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255, 229, 243, 255),
+            ),
+            margin: const EdgeInsets.all(10.0),
+            child: const TextField(
+              style: TextStyle(
+                fontSize: 12,
+              ),
+              obscureText: true,
+              decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  labelText: "Password",
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  errorBorder: InputBorder.none),
+            ),
+          ),
+
+          // The Remember Me and Forget Password row
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    const Text(
+                      "Remember Me",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: 13.0),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10.0, 0),
+                  child: const Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 13.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // The Sign In button
+          Container(
+            height: 40.0,
+            width: 470.0,
+            margin: const EdgeInsets.all(5.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: const Text("Sign In"),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Don't have an account? ",
+                  style: TextStyle(fontSize: 13),
+                ),
+                Text(
+                  "Sign up",
+                  style: TextStyle(color: Colors.green, fontSize: 13),
+                )
+              ],
+            ),
+          ),
+
+          // Terms and Conditions
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                    "By clicking 'Continue with Email/Apple/Google/Facebook'",
+                    style: TextStyle(fontSize: 10.0)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("above, you agree to",
+                        style: TextStyle(fontSize: 10.0)),
+                    Text("Terms and Conditions ",
+                        style: TextStyle(color: Colors.amber, fontSize: 10.0)),
+                    Text("and", style: TextStyle(fontSize: 10.0)),
+                    Text(
+                      " Privacy Policy",
+                      style: TextStyle(color: Colors.amber, fontSize: 10.0),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
