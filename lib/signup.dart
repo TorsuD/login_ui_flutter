2eedba884fe8d641da71bca2c828e194b57237b2
Derrick Torsu <torsuderrick2004@gmail.com>
2022-11-02 23:50:30 +0000

    Modified the sign up page

--- lib/signup.dart
+++ lib/signup.dart
@@ -6,7 +6,7 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Sign up page"),
+      body: Text("The Sign up page"),
     );
   }
 }
